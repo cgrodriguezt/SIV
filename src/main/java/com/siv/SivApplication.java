b12c8d544b74b0792947a54d3a16fc3de83f1da9
YurleyM <molinayurley72@gmail.com>
2024-04-03 19:17:24 -0500

    correccion codigo

---
+++ src/main/java/com/siv/SivApplication.java
@@ -0,0 +1,13 @@
+package com.siv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivApplication.class, args);
+	}
+
+}
